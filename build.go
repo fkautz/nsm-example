72ba39298e103295f758e59ebc6a55fd9a3a769d
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2020-09-05 23:29:17 -0700

    add generic spire identity
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>

--- build.go
+++ build.go
@@ -68,6 +68,15 @@ func Install() error {
 	return nil
 }
 
+func Register() error {
+	fmt.Println("Registering SPIRE identities")
+
+	argString := "exec -n spire spire-server-0 -- /opt/spire/bin/spire-server entry create -spiffeID spiffe://example.org/ns/default/sa/default -parentID spiffe://example.org/ns/spire/sa/spire-agent -selector k8s:ns:default -selector k8s:sa:default"
+	args := strings.Split(argString, " ")
+
+	return sh.RunV("kubectl", args...)
+}
+
 // Clean up after yourself
 func Clean() {
 	fmt.Println("Cleaning...")
